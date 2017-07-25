20abe2be099807dd2a6e5c27ea4a4467e343b54d
Daniel <narselldev@gmail.com>
2017-07-24 21:20:03 -0500

    Setting up for tank movement. Inverted the camera control with the game controller and moved it to the right stick.

---
+++ TankGame/Source/TankGame/TankTrack.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to set maximun driving force and control its behavior.
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TANKGAME_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	
+};
