5114af0b57a4742995e670931251a504248cf4de
Daniel <narselldev@gmail.com>
2017-07-25 00:02:10 -0500

    Player Tank now moves using AddForceAtLocation(). Added Tank physics materials to reduce friction. Enhanced box colliders on tank again.

--- TankGame/Source/TankGame/TankTrack.cpp
+++ TankGame/Source/TankGame/TankTrack.cpp
@@ -8,6 +8,13 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle %f"), *Name, Throttle);
 
 	///TODO Clamp actual value
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
 }
 
 
--- TankGame/Source/TankGame/TankTrack.h
+++ TankGame/Source/TankGame/TankTrack.h
@@ -19,5 +19,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	 
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; 
 };
